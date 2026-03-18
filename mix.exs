8a0c716fc7a4cd556b95c29a58768f4a706d31e1
Anson MacKeracher <anson@mackeracher.com>
2026-03-18 15:21:37 -0700

    chore: bootstrap local services and initial Phoenix mix scaffold

---
+++ mix.exs
@@ -0,0 +1,58 @@
+defmodule Automaton.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :automaton,
+      version: "0.1.0",
+      elixir: "~> 1.16",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
+      deps: deps(),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers()
+    ]
+  end
+
+  def application do
+    [
+      mod: {Automaton.Application, []},
+      extra_applications: [:logger, :runtime_tools]
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp deps do
+    [
+      {:bandit, "~> 1.5"},
+      {:ecto_sql, "~> 3.11"},
+      {:floki, ">= 0.30.0", only: :test},
+      {:gettext, "~> 0.24"},
+      {:jason, "~> 1.4"},
+      {:oban, "~> 2.17"},
+      {:phoenix, "~> 1.7.14"},
+      {:phoenix_ecto, "~> 4.5"},
+      {:phoenix_html, "~> 4.1"},
+      {:phoenix_live_reload, "~> 1.5", only: :dev},
+      {:phoenix_live_view, "~> 1.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:tailwind, "~> 0.2", runtime: Mix.env() == :dev},
+      {:telemetry_metrics, "~> 1.0"},
+      {:telemetry_poller, "~> 1.0"}
+    ]
+  end
+
+  defp aliases do
+    [
+      setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
+      "ecto.setup": ["ecto.create", "ecto.migrate"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      "assets.setup": ["tailwind.install --if-missing"],
+      "assets.build": ["tailwind default"],
+      "assets.deploy": ["tailwind default --minify", "phx.digest"]
+    ]
+  end
+end
